40f304454061b24388eaf45e965aef52c6b2e5f7
Mikael Ahonen <mikael.ahonen@hotmail.com>
2019-01-01 18:49:27 +0200

    Add modules to PYTHONPATH.

--- env_example.sh
+++ env_example.sh
@@ -1,4 +1,5 @@
 export PROJECT_DIR='/mnt/c/Users/Your Name/Desktop/Projects/your_repository'
+export PYTHONPATH="${PYTHONPATH}:$PROJECT_DIR/modules"
 cd $PROJECT_DIR
 source venv/bin/activate
 pip3 install -r requirements.txt
